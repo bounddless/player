467c408992c2c31e313e9a2a6530691e1782ec72
Egor <kegor201207@gmail.com>
2025-06-17 18:49:46 +0300

    123

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,11 @@
 import './styles'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-<<<<<<< Updated upstream
 import Header from './layouts/Header/Header.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-=======
-// import App from './App.jsx'
-
-import Header from './layouts/Header/Header.jsx';
-import Fest from '@/layouts/Fest/Fest.jsx';
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-
-      <Fest/>
-    <Header/>
->>>>>>> Stashed changes
   </StrictMode>,
     
 
